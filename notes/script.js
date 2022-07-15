4f32e913380d16b62f98dfa1583fd62447b2fc16
Allan <martinezallan123@gmail.com>
2022-07-15 14:50:06 -0700

    notes app inital commit

---
+++ notes/script.js
@@ -0,0 +1,46 @@
+
+const textArea = document.querySelector('textarea')
+const save = document.querySelector('.save');
+const trash = document.querySelector('.trash')
+const add = document.querySelector('.add')
+
+save.addEventListener('click',storetext);
+trash.addEventListener('click',destroy);
+add.addEventListener('click',createNote);
+
+function storetext(){
+    textArea.disabled ? textArea.disabled = false : textArea.disabled = true; //toggle disabled attribute
+
+}
+
+function destroy(){
+    this.parentElement.parentElement.remove()
+
+}
+
+function createNote(){
+    const note = document.querySelector('.note')
+    const noteCopy = note.cloneNode(true)
+    note.style.display = 'inline'
+    if(noteCopy.querySelector('textarea').disabled == true){    //clears disabled when a copy is made
+        noteCopy.querySelector('textarea').disabled = false
+    }
+
+    if(noteCopy.querySelector('textarea').value.length > 0){    //clears text when a copy is made
+        noteCopy.querySelector('textarea').value = ''
+    }
+    
+    const textArea = noteCopy.querySelector('textarea')
+    const save = noteCopy.querySelector('.save');
+    const trash = noteCopy.querySelector('.trash')
+
+    save.addEventListener('click',()=>{
+        textArea.disabled ? textArea.disabled = false : textArea.disabled = true;
+
+    });
+    trash.addEventListener('click',destroy);
+    add.addEventListener('click',createNote);
+    
+    document.body.prepend(noteCopy)
+
+}
